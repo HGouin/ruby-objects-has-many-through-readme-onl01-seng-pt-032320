c7dc75cbeaed56b414e91a0e7a302685be58c598
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-23 11:59:06 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -16,14 +16,14 @@ class Customer
   def meals
     Meal.all.select do |meal|
       meal.customer == self
-    end
   end
+
 
   def waiters
     meals.map do |meal|
       meal.waiter
-    end
   end
+
 
   def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
